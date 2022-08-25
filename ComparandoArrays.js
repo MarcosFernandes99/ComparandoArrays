7eb35d0698fddfc8e8484a718409babb260ce420
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-25 14:54:38 -0300

    Criando algoritmo de compara

---
+++ ComparandoArrays.js
@@ -0,0 +1,12 @@
+// DESAFIO DE COMPARAÇÃO DE ARRAYS P/ EXTRAIR SOMENTE OS NUMEROS QUE REPETEM ENTRE ELES
+
+var arrayPrimeiro = []
+var arraySegundo = []
+
+for(var contador = 0; contador < 10; contador++){
+    arrayPrimeiro[contador] = parseInt(prompt(`Insira um numero no 1º Array`))
+    arraySegundo[contador] = parseInt(prompt(`Insira um numero no 2º Array`))
+}
+console.log(arrayPrimeiro)
+console.log(arraySegundo)
+
