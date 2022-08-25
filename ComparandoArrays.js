17c291b3dcb7e26308abd6d570dc7c703403153d
Marcos Fernandes <marcos_fernandes94@hotmail.com>
2022-08-25 15:03:44 -0300

    Finalizado algoritmo de comparação entre arrays

--- ComparandoArrays.js
+++ ComparandoArrays.js
@@ -1,7 +1,9 @@
-// DESAFIO DE COMPARAÇÃO DE ARRAYS P/ EXTRAIR SOMENTE OS NUMEROS QUE REPETEM ENTRE ELES
+// DESAFIO DE COMPARAÇÃO DE ARRAYS P/ EXTRAIR SOMENTE OS NUMEROS QUE REPETEM ENTRE ELES E SE FOR MAIOR QUE 5
 
 var arrayPrimeiro = []
 var arraySegundo = []
+var arrayTerceiro = []
+var contadorTerceiro = 0
 
 for(var contador = 0; contador < 10; contador++){
     arrayPrimeiro[contador] = parseInt(prompt(`Insira um numero no 1º Array`))
@@ -10,3 +12,12 @@ for(var contador = 0; contador < 10; contador++){
 console.log(arrayPrimeiro)
 console.log(arraySegundo)
 
+for(var primeiro = 0; primeiro < contador; primeiro++){
+    for(var segundo = 0; segundo < contador; segundo++){
+        if(arrayPrimeiro[primeiro] == arraySegundo[segundo] && arrayPrimeiro[primeiro] > 5){
+            arrayTerceiro[contadorTerceiro] = arrayPrimeiro[primeiro]
+            contadorTerceiro++
+        }
+    }
+}
+console.log(arrayTerceiro)
